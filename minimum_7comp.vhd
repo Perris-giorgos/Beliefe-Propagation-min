b620fa527389379f9528a2b1a7a79a03c639ce36
perris-giorgos <giorperr91@mail.com>
2016-10-27 11:35:36 +0300

    first comit

---
+++ minimum_7comp.vhd
@@ -0,0 +1,20 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity minimum_7comp is
+end minimum_7comp;
+
+architecture Behavioral of minimum_7comp is
+
+begin
+
+
+end Behavioral;
+
